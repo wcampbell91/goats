0b6be982eeca5984ffbb5df68347431c44a54fc7
Billy Campbell <wcampbell91@gmail.com>
2020-08-11 18:43:33 -0500

    propTypes chit-chat

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Goat from '../Goat/Goat';
 
+import goatShape from '../../helpers/propz/goatShape';
+
 class GoatCorral extends React.Component {
+  static propTypes = {
+    // ^^ Expects static propTypes
+    goats: PropTypes.arrayOf(goatShape.goatShape),
+    // need the PropTypes here because we are working with an array of objects, so we need to use the arrayOf() method...
+  }
+
   render() {
     const { goats } = this.props;
 
---
+++ src/helpers/propz/goatShape.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const goatShape = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string.isRequired,
+  isbusy: PropTypes.bool.isRequired,
+});
+
+// ^^ This defines what will be expected of a prop that is passed to a component, so you need to add static propType = {} to wherever is receving a prop
+// AKA GoatCorral.js and Goat.js
+
+export default { goatShape };
